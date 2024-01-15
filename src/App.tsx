465c961cbc75be4547342e809cc944f8cadc8c0f
Wagner Rodrigues <wagner92dev@gmail.com>
2024-01-15 19:30:26 -0300

    Setup inicial

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-full flex flex-col">
+      <h1 className="text-blue-600 font-semibold text-2xl">
+        Bem vindos à pokedex
+      </h1>
     </div>
   );
 }
