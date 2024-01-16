3fb3764f48650b29df971e440dfefaaa514718cf
Wagner Rodrigues <wagner92dev@gmail.com>
2024-01-16 00:28:53 -0300

    useState e useRef

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
+import { useState } from "react";
 import { Filho } from "./Filho";
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <div className="w-full flex flex-col">
       <h1 className="text-blue-600 font-semibold text-2xl">
         Bem vindos à pokedex
       </h1>
+
+      <label>{count}</label>
+      <button 
+        onClick={() => setCount((prev) => prev + 1)}
+        className="bg-blue-600 hover:bg-blue-400 w-24 text-white"
+      >
+        Incrementar
+      </button>
 
       <hr />
 
--- src/Filho.tsx
+++ src/Filho.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from 'react';
+import { ReactNode, useRef } from 'react';
 
 interface FilhoProps{
   text?: string;
@@ -6,10 +6,29 @@ interface FilhoProps{
 }
 
 export const Filho = ({text, children}: FilhoProps) => {
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  const reduceInputSize = () => {
+    if (inputRef && inputRef.current && inputRef.current.style) {
+      inputRef.current.style.width = '100px'
+    }
+  }
+
   return (
-    <>
+    <div className='w-full flex flex-col'>
       <h2>{text}</h2>
       {children}
-    </>
+      <input
+         ref={inputRef}
+         type='text'
+         placeholder='Digite aqui'
+         className='bg-gray-200 border rounded-md'
+      />
+      <button 
+        onClick={reduceInputSize}
+        className="bg-red-600 hover:bg-red-400  text-white">
+        Diminuir tamanho do input
+      </button>
+    </div>
   );
 };
